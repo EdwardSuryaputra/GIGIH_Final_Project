7eb52d3d7bf1ff07b5c43325cda2270dec317fa3
Edward Suryaputra <65845434+EdwardSuryaputra@users.noreply.github.com>
2022-04-15 09:21:10 +0000

    Add validation: is invalid if description's character is more than 150

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,5 @@
 class Menu < ApplicationRecord
   validates :item_name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0.01}
+  validates :description, length: {maximum: 150}
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 require 'factory_bot'
 
 RSpec.describe Menu, type: :model do
-  it 'is valid with a name and a price' do
-    expect(FactoryBot.build(:menu, description: nil)).to be_valid
-  end
-
-  it 'is invalid without a name' do
-    menu = FactoryBot.build(:menu, item_name: nil)
-    menu.valid?
-    expect(menu.errors[:item_name]).to include("can't be blank")
-  end
-
-  it 'is invalid without a price' do
-    menu = FactoryBot.build(:menu, price: nil)
-    menu.valid?
-    expect(menu.errors[:price]).to include("can't be blank")
+  describe 'invalid or valid if attribute blank' do
+    it 'is valid with a name and a price' do
+      expect(FactoryBot.build(:menu, description: nil)).to be_valid
+    end
+  
+    it 'is invalid without a name' do
+      menu = FactoryBot.build(:menu, item_name: nil)
+      menu.valid?
+      expect(menu.errors[:item_name]).to include("can't be blank")
+    end
+  
+    it 'is invalid without a price' do
+      menu = FactoryBot.build(:menu, price: nil)
+      menu.valid?
+      expect(menu.errors[:price]).to include("can't be blank")
+    end
   end
   
   it "is invalid with price less than 0.01" do
@@ -32,6 +34,14 @@ RSpec.describe Menu, type: :model do
 
     expect(menu2.errors[:item_name]).to include("has already been taken")
   end
+
+  it "is invalid if description's character is more than 150" do
+        menu = FactoryBot.build(:menu, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+        menu.valid?
+        expect(menu.errors[:description]).to include("is too long (maximum is 150 characters)")
+  end
+
+  
 
   
 end
