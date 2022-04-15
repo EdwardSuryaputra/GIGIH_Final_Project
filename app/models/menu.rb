f4ded40e4501745c83701a0d23d386f178d02c60
Edward Suryaputra <65845434+EdwardSuryaputra@users.noreply.github.com>
2022-04-15 08:53:34 +0000

    Add validation: invalid if name or price is nil

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,4 @@
 class Menu < ApplicationRecord
+  validates :item_name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0.01}
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 require 'factory_bot'
 
 RSpec.describe Menu, type: :model do
-   it "is invalid with price less than 0.01" do
-        menu = build(:menu, price: 0)
+  it 'is valid with a name and a price' do
+    expect(FactoryBot.build(:menu, description: nil)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    menu = FactoryBot.build(:menu, item_name: nil)
+    menu.valid?
+    expect(menu.errors[:item_name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a price' do
+    menu = FactoryBot.build(:menu, price: nil)
+    menu.valid?
+    expect(menu.errors[:price]).to include("can't be blank")
+  end
+  
+  it "is invalid with price less than 0.01" do
+        menu = FactoryBot.build(:menu, price: 0)
         menu.valid?
         expect(menu.errors[:price]).to include("must be greater than 0.01")
-    end
+  end
 
   
 end
