0523dc037d51957a3c6d18aecd3e73c6fe131570
Edward Suryaputra <65845434+EdwardSuryaputra@users.noreply.github.com>
2022-04-17 04:01:05 +0000

    Add validation: menu, order, price and quantity can't be blank

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,10 @@
 class OrderDetail < ApplicationRecord
   belongs_to :menu
   belongs_to :order
+
+  validates :menu_id, presence: true
+  validates :order_id, presence: true
+  validates :unit_price, presence: true
+  validates :quantity, presence: true
+
 end
--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe OrderDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'invalid if attribute blank' do
+    it 'is invalid without a menu id' do
+      order = FactoryBot.build(:order_detail, order_id: 1)
+      order.valid?
+      expect(order.errors[:menu_id]).to include("can't be blank")
+    end
+  
+    it 'is invalid without an order id' do
+      order = FactoryBot.build(:order_detail, menu_id: 1)
+      order.valid?
+      expect(order.errors[:order_id]).to include("can't be blank")
+    end
+
+    it 'is invalid without a unit price' do
+      order = FactoryBot.build(:order_detail, menu_id: 1, order_id: 1, unit_price: nil)
+      order.valid?
+      expect(order.errors[:unit_price]).to include("can't be blank")
+    end
+
+    it 'is invalid without a quantity' do
+      order = FactoryBot.build(:order_detail, menu_id: 1, order_id: 1, quantity: nil)
+      order.valid?
+      expect(order.errors[:quantity]).to include("can't be blank")
+    end
+  end
 end
